faa0d07ade074a81bba1dc2fd53031d59258bb4b
tincan81 <quentin.puel@ynov.com>
2018-12-19 17:31:45 +0100

    new file globalstat delete primary and secondary stage

---
+++ New Unity Project/Assets/Combat_resources/GlobalStats.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+    * @author Nummy
+    * @version 0.1
+    **/
+class GlobalStats
+{
+    //Primary Stats
+	public int Force { get; set; }
+	public int Dexterite { set; get; }
+	public int Inteligence { set; get; }
+	public int Sagesse { set; get; }
+	public int Charisme { set; get; }
+	public int Constitution { set; get; }
+
+    //Secondary stats
+    public int HealthPoint { get; set; }
+    public int Mana { set; get; }
+    public int Stamina { set; get; }
+    public int ResistanceDamageM { set; get; }
+    public int ResistanceDamageP { set; get; }
+    public int ResistanceMental { set; get; }
+    /**
+    * @author Nummy
+    * @
+    **/
+    public GlobalStats(int f , int d, int i, int s, int c, int ch)
+	{
+        this.Force = f;
+        this.Dexterite = d;
+        this.Inteligence = i;
+        this.Sagesse = s;
+        this.Charisme = ch;
+        this.Constitution = c;
+        
+    }
+
+
+}
